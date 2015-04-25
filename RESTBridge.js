96927a3d66baf893867dc9efcf11a17cb6ad4f0c
David Janes <davidjanes@davidjanes.com>
2015-04-25 10:49:15 -0400

    remove old documentation dups

--- RESTBridge.js
+++ RESTBridge.js
@@ -36,16 +36,10 @@ var logger = bunyan.createLogger({
 });
 
 /**
- *  EXEMPLAR and INSTANCE
+ *  See {iotdb.bridge.Bridge#Bridge} for documentation.
  *  <p>
- *  No subclassing needed! The following functions are
- *  injected _after_ this is created, and before .discover and .connect
- *  <ul>
- *  <li><code>discovered</code> - tell IOTDB that we're talking to a new Thing
- *  <li><code>pulled</code> - got new data
- *  <li><code>connected</code> - this is connected to a Thing
- *  <li><code>disconnnected</code> - this has been disconnected from a Thing
- *  </ul>
+ *  @param {object|undefined} native
+ *  only used for instances, should be 
  */
 var RESTBridge = function (initd, native) {
     var self = this;
@@ -78,13 +72,7 @@ RESTBridge.prototype.name = function () {
 /* --- lifecycle --- */
 
 /**
- *  EXEMPLAR.
- *  Discover WeMo Socket
- *  <ul>
- *  <li>look for Things (using <code>self.bridge</code> data to initialize)
- *  <li>find / create a <code>native</code> that does the talking
- *  <li>create an RESTBridge(native)
- *  <li>call <code>self.discovered(bridge)</code> with it
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.discover = function () {
     var self = this;
@@ -102,8 +90,7 @@ RESTBridge.prototype.discover = function () {
 };
 
 /**
- *  INSTANCE
- *  This is called when the Bridge is no longer needed. When
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.connect = function (connectd) {
     var self = this;
@@ -150,8 +137,7 @@ RESTBridge.prototype._forget = function () {
 };
 
 /**
- *  INSTANCE and EXEMPLAR (during shutdown).
- *  This is called when the Bridge is no longer needed. When
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.disconnect = function () {
     var self = this;
@@ -165,8 +151,7 @@ RESTBridge.prototype.disconnect = function () {
 /* --- data --- */
 
 /**
- *  INSTANCE.
- *  Send data to whatever you're taking to.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.push = function (pushd) {
     var self = this;
@@ -202,9 +187,7 @@ RESTBridge.prototype.push = function (pushd) {
 };
 
 /**
- *  INSTANCE.
- *  Pull data from whatever we're talking to. You don't
- *  have to implement this if it doesn't make sense
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.pull = function () {
     var self = this;
@@ -218,18 +201,7 @@ RESTBridge.prototype.pull = function () {
 /* --- state --- */
 
 /**
- *  INSTANCE.
- *  Return the metadata - compact form can be used.
- *  Does not have to work when not reachable
- *  <p>
- *  Really really useful things are:
- *  <ul>
- *  <li><code>iot:thing</code> required - a unique ID
- *  <li><code>iot:device</code> suggested if linking multiple things together
- *  <li><code>schema:name</code>
- *  <li><code>iot:number</code>
- *  <li><code>schema:manufacturer</code>
- *  <li><code>schema:model</code>
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.meta = function () {
     var self = this;
@@ -244,20 +216,14 @@ RESTBridge.prototype.meta = function () {
 };
 
 /**
- *  INSTANCE.
- *  Return True if this is reachable. You
- *  do not need to worry about connect / disconnect /
- *  shutdown states, they will be always checked first.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.reachable = function () {
     return (this.native !== null) && this._reachable;
 };
 
 /**
- *  INSTANCE.
- *  Configure an express web page to configure this Bridge.
- *  Return the name of the Bridge, which may be
- *  listed and displayed to the user.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 RESTBridge.prototype.configure = function (app) {};
 

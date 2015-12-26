f708f5b069fc567b7f0b9ec31ddf12d78c907810
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:48 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -36,10 +36,8 @@ echo "=================="
         RESTBridge.js index.js \
         models/*.js models/*.json \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
 
-    cd "${NPM_DST}" || exit 1
-    npm publish || exit 1
     git commit -m "new release" package.json || exit 1
     git push || exit 1
 
